ef62b280103f7cef417109898b09612d65dc3fe6
AhnJunje <87896157+annjj322@users.noreply.github.com>
2024-03-15 16:13:50 +0900

    Title: 그 계절, 그 날, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240315/그 계절, 그 날/that-season-that-day.py
+++ 240315/그 계절, 그 날/that-season-that-day.py
@@ -7,14 +7,16 @@ summer = [6,7,8]
 fall = [9,10,11]
 winter = [12,1,2]
 
-if y % 4 == 0 and m == 2:
-    if y % 100 == 0:
-        if y % 400 == 0:
-            yoon = True
+if y % 4 == 0 and m == 2: # 윤년, 2월 체크
+    if y % 100 == 0: # 100으로 나눠지면서
+        if y % 400 == 0: # 동시에 400으로 나눠지면
+            yoon = True 윤년
     else:
-        yoon = True
+        yoon = True # 100으로 안나눠지면 윤년
 
 if yoon == False and m == 2 and d > 28:
+    print(-1)
+elif yoon == True and m == 2 and d > 29:
     print(-1)
 elif m in long_month and d > 31:
     print(-1)
