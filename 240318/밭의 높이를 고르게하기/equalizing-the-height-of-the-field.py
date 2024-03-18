5b4e1717df462a5232b00284d7c52e7ce835072b
AhnJunje <87896157+annjj322@users.noreply.github.com>
2024-03-18 15:33:45 +0900

    Title: 밭의 높이를 고르게하기, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240318/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240318/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -3,7 +3,7 @@ array_n = list(map(int,input().split()))
 minus_array = []
 for num in array_n:
     minus_array.append(abs(num-h))
-
+print(minus_array)
 sum_array = []
 tmp = 0
 for j in range(len(minus_array)-t+1):
@@ -11,4 +11,5 @@ for j in range(len(minus_array)-t+1):
         tmp += minus_array[i+j]
     sum_array.append(tmp)
     tmp = 0
+print(sum_array)
 print(min(sum_array))
