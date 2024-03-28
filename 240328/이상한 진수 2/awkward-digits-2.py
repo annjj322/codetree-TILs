d8a1c4aef081a241cad54e51d847246d3661ab47
AhnJunje <87896157+annjj322@users.noreply.github.com>
2024-03-28 21:46:39 +0900

    Title: 이상한 진수 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240328/이상한 진수 2/awkward-digits-2.py
+++ 240328/이상한 진수 2/awkward-digits-2.py
@@ -4,9 +4,14 @@ binary = bin(int(n,2))
 for num in binary[2:]:
     array.append(num)
 
-for i in range(len(array)):
-    if array[i] == '0':
-        array[i] = '1'
-        break
+if '0' not in array:
+    array[-1] = '0'
+else:
+    for i in range(len(array)):
+        if array[i] == '0':
+            array[i] = '1'
+            break
+    
+
 result = ''.join(map(str,array))
 print(int(result,2))
