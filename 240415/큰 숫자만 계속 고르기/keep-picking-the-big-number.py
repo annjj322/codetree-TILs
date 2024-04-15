fe88edc6ff7936b3bbdb56d85529d32df9d547f2
AhnJunje <87896157+annjj322@users.noreply.github.com>
2024-04-15 17:44:55 +0900

    Title: 큰 숫자만 계속 고르기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240415/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
+++ 240415/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
@@ -7,5 +7,8 @@ for num in array:
     heapq.heappush(prique,-num)
 
 for _ in range(n):
-    prique[0] += 1
-    print(prique)
+    max_val = heapq.heappop(prique)
+    max_val += 1
+    heapq.heappush(prique, max_val)
+
+print(prique[0])
